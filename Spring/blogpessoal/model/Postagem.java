84beec6c8f4232d6feb5634d3d14bbbfb97b3651
Karine <55106735+ohtoshi@users.noreply.github.com>
2022-03-28 08:44:27 -0300

    PUT & DELETE

---
+++ Spring/blogpessoal/model/Postagem.java
@@ -0,0 +1,66 @@
+package org.generation.blogpessoal.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity  // anotação, indica que vai virar tabela transformo em tabela
+@Table(name="tb_postagens")  // nomeia a tabela
+public class Postagem {
+
+	@Id  // informa que é um ID
+	@GeneratedValue(strategy= GenerationType.IDENTITY) // auto increment
+	private Long id; // Long maiúsculo pois é tipo primitivo, como String
+	
+	@NotNull
+	@Size(min=5, max=50) // validação de quantidade de caracteres
+	private String titulo;
+	
+	@Size(min=5, max=500) // validação de quantidade de caracteres
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDate data;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
+}
